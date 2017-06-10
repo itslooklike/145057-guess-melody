509063758eb099fbfe919c1e234156d1cea4cfae
a.masalov <delsol@bk.ru>
2017-06-10 12:32:14 +0300

    добавил тесты

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const rollup = require('gulp-better-rollup');
 const sourcemaps = require('gulp-sourcemaps');
+const mocha = require('gulp-mocha');
 
 gulp.task('style', function () {
   gulp.src('sass/style.scss')
@@ -89,6 +90,15 @@ gulp.task('serve', ['assemble'], function () {
     open: true,
     port: 3502,
     ui: false
+  });
+
+  gulp.task('test', function () {
+    return gulp
+      .src(['js/**/*.test.js'], { read: false })
+      .pipe(mocha({
+        compilers: ['js:babel-register'],
+        reporter: 'nyan'
+      }));
   });
 
   gulp.watch('sass/**/*.{scss,sass}', ['style']);
